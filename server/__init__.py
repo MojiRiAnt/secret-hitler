c53f7bd45980fe7e8fadbdf35ee78b269ed2b6f2
MojiRiAnt <DmiMatth@gmail.com>
2018-11-22 16:42:12 +0200

    Add some HTML

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,11 @@
 from flask import (Flask,)
 
 app = Flask (
-    __name__,
+    __name__.split('.')[0],
     template_folder='resources/templates',
+    static_folder='resources',
 )
+
 app.config.update({
     "FLASK_ENV" : "development",
 })
--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,7 @@
 from flask import (render_template,)
 from server import (app,)
 
-@app.route('/hitler/create')
-def hitler_create():
-    return render_template('test.html', route_token='hitler-create')
+@app.route('/host')
+def host():
+    return render_template('host.html')
 
